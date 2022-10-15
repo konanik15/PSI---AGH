6aeb93b2ffb14d9e2f0470e0548f779b7d295f24
Daniel_S <69630750+konannik@users.noreply.github.com>
2022-10-15 14:28:03 +0200

    Initial commit

---
+++ src/main/java/pl/agh/grupa2/PSIOCR/PsiOcrApplication.java
@@ -0,0 +1,13 @@
+package pl.agh.grupa2.PSIOCR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiOcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsiOcrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/agh/grupa2/PSIOCR/PsiOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.agh.grupa2.PSIOCR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsiOcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
